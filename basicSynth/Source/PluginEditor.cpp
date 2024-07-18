43626833f58a68eafc43049580a37b94590d0180
Oliver Camp <84264585+Blahdude@users.noreply.github.com>
2024-07-17 21:10:51 -0700

    Update PluginEditor.cpp

--- basicSynth/Source/PluginEditor.cpp
+++ basicSynth/Source/PluginEditor.cpp
@@ -12,24 +12,32 @@
 #include "PluginEditor.h"
 
 //==============================================================================
+# constructor definition, connecting audio to gui
+# creating instances of variabls, passing adding audio to each one
 BasicSynthAudioProcessorEditor::BasicSynthAudioProcessorEditor (BasicSynthAudioProcessor& p)
 : juce::AudioProcessorEditor (&p), processor (p), m_OscGUI(p), m_adsrGUI(p), m_filterGUI(p) {
 
     setSize (600, 300);
-    
+
+    # makes visible the gui instances
     addAndMakeVisible(&m_OscGUI);
     addAndMakeVisible(&m_adsrGUI);
     addAndMakeVisible(&m_filterGUI);
 }
 
+# desconstructor
+# instances will be automatically destroyed, so no extra cleanup is necessary
 BasicSynthAudioProcessorEditor::~BasicSynthAudioProcessorEditor() {
 }
 
 //==============================================================================
 void BasicSynthAudioProcessorEditor::paint (juce::Graphics& g) {
+    # filling the background to black
+    # more complex GUIs might draw additional elements here like adding images and designs
     g.fillAll(juce::Colours::black); 
 }
 
+# ensures that the plugin looks good after being resized
 void BasicSynthAudioProcessorEditor::resized() {
     juce::Rectangle<int> area = getLocalBounds();
     
